b7eb1ad0200466ed3632dc2b2cb5110e280bf22e
Rajeshkumar <38863729+nrajeshk93@users.noreply.github.com>
2026-08-31 05:41:24 +0530

    new

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hotelbellelite.hbe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0"
     }
 
     buildTypes {
